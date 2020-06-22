8e94311bb1609c512f97fda4e4bf2798cabc7ea0
melnik <kivals.90@yandex.ru>
2020-06-22 10:42:07 +0300

    Solution for 1-module/1-task

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -2,3 +2,17 @@ import Vue from '/vendor/vue.esm.browser.js';
 
 // const app = ...
 // Рекомендуется использовать МЕТОД в качестве обработчика события
+
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    buttonValue: 0,
+  },
+
+  methods: {
+    counter() {
+      return this.buttonValue++;
+    },
+  },
+});
