55e18a956a4826da88c0b1f0e0e14a9b545d230e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-08-26 09:14:27 +0300

    Added task Layouts

---
+++ 4-module/5-task/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+      <router-link :to="{ name: 'meetups' }">Митапы</router-link> |
+      <router-link :to="{ name: 'form' }">Создать митап</router-link> |
+      <router-link :to="{ name: 'login' }">Вход</router-link>
+    </nav>
+    <base-layout id="app">
+      <router-view />
+    </base-layout>
+  </div>
+</template>
+
+<script>
+import BaseLayout from './components/BaseLayout';
+export default {
+  name: 'App',
+  components: { BaseLayout },
+  created() {
+    this.$router.replace('/');
+  },
+};
+</script>
+
+<style scoped></style>
