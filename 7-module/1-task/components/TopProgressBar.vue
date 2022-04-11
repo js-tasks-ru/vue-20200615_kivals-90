ccfa0869bfd502465386d77875663b54509d1efa
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-04-11 18:50:32 +0300

    Added solution for task: TopProgressBar

--- 7-module/1-task/components/TopProgressBar.vue
+++ 7-module/1-task/components/TopProgressBar.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="progress"></div>
+  <div
+    class="progress"
+    :class="{
+      show,
+      failed,
+    }"
+    :style="{
+      width: currentProgress + '%',
+    }"
+  ></div>
 </template>
 
 <script>
@@ -8,8 +17,51 @@ const K = 0.975;
 const STEP_DURATION = 200;
 const HIDE_DURATION = 200;
 
+let interval = null;
+
 export default {
   name: 'TopProgressBar',
+
+  data() {
+    return {
+      currentProgress: 0,
+      show: false,
+      failed: false,
+    };
+  },
+
+  methods: {
+    start() {
+      this.currentProgress = 0;
+      this.show = true;
+      this.failed = false;
+      if (interval) {
+        clearInterval(interval);
+      }
+      this.currentProgress = 0;
+      interval = setInterval(() => {
+        this.currentProgress =
+          MAX_PROGRESS - (MAX_PROGRESS - this.currentProgress) * K;
+      }, STEP_DURATION);
+    },
+
+    async finish() {
+      clearInterval(interval);
+      interval = null;
+      this.currentProgress = 100;
+      setTimeout(() => {
+        this.show = 0;
+        setTimeout(() => {
+          this.currentProgress = 0;
+        }, HIDE_DURATION + 100);
+      }, STEP_DURATION + 200);
+    },
+
+    fail() {
+      this.failed = true;
+      this.finish();
+    },
+  },
 };
 </script>
 
