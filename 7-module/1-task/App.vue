9d5e63d81911f799272fb040cf913abce980afea
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-04-11 18:50:25 +0300

    Added task TopProgressBar

---
+++ 7-module/1-task/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <top-progress-bar ref="topProgressBar" />
+    <main class="page container">
+      <p><button @click="start">Start</button></p>
+      <p><button @click="finish">Finish</button></p>
+      <p><button @click="fail">Fail</button></p>
+    </main>
+  </div>
+</template>
+
+<script>
+import TopProgressBar from './components/TopProgressBar';
+
+export default {
+  name: 'App',
+  components: { TopProgressBar },
+
+  methods: {
+    start() {
+      this.$refs['topProgressBar'].start();
+    },
+
+    finish() {
+      this.$refs['topProgressBar'].finish();
+    },
+
+    fail() {
+      this.$refs['topProgressBar'].fail();
+    },
+  },
+};
+</script>
+
+<style>
+p {
+  margin: 1rem auto;
+}
+</style>
---
+++ 7-module/1-task/components/TopProgressBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="progress"></div>
+</template>
+
+<script>
+const MAX_PROGRESS = 95;
+const K = 0.975;
+const STEP_DURATION = 200;
+const HIDE_DURATION = 200;
+
+export default {
+  name: 'TopProgressBar',
+};
+</script>
+
+<style scoped>
+.progress {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 4px;
+  width: 0;
+  opacity: 0;
+  transition: opacity 0.2s linear;
+  background-color: #4c6bb6;
+  z-index: 999999;
+}
+
+.progress.show {
+  opacity: 1;
+  transition: width 0.2s linear;
+}
+
+.progress.failed {
+  background-color: #db3851;
+}
+</style>
